368a97ba9ae46b0002ed3fda779c41faf98c9f2c
João Neto <121770060+JaoNeto26@users.noreply.github.com>
2023-09-13 15:01:50 -0300

    Add files via upload
    teste

---
+++ programa/conquista.js
@@ -0,0 +1,16 @@
+// Quando a condição é completada da Fase 1
+function completarCondicaoFs1() {
+    localStorage.setItem("Fase1Completa", "true");
+}
+// Quando a condição é completada da Fase 2
+function completarCondicaoFs2() {
+    localStorage.setItem("Fase2Completa", "true");
+}
+// Quando a condição é completada Fase 3
+function completarCondicaoFs3() {
+    localStorage.setItem("Fase3Completa", "true");
+}
+// Quando a condição é completada Fase 4
+function completarCondicaoFs4() {
+    localStorage.setItem("Fase4Completa", "true");
+}
